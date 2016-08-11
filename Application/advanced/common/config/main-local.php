2b8383052e7fbd3d1e399eb3e87c78164c140fa7
owenylaya <odylaya@student.apc.edu.ph>
2016-08-11 16:28:32 +0800

    advaned yii installed

--- Application/advanced/common/config/main-local.php
+++ Application/advanced/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=orders',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
