99fba890ade0e0adbc6efaaf66ed3f1afd8aa09a
owenylaya <odylaya@student.apc.edu.ph>
2016-08-12 03:07:48 +0800

    modified

--- Application/advanced/frontend/views/site/neworder.php
+++ Application/advanced/frontend/views/site/neworder.php
@@ -1,4 +1,16 @@
-<?php 
-asdasdasdasd
+<?php
 
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'New Order';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="site-neworder">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>This is the About page. You may modify the following file to customize its content:</p>
+
+    <code><?= __FILE__ ?></code>
+</div>
