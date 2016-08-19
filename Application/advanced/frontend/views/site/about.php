a5343780a019dd4255707fcef151d4aae657dc37
owenylaya <odylaya@student.apc.edu.ph>
2016-08-19 08:50:05 +0800

    buttons

--- Application/advanced/frontend/views/site/about.php
+++ Application/advanced/frontend/views/site/about.php
@@ -7,12 +7,34 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>This is the About page. You may modify the following file to customize its content:</p>
+	
+	<a href='#' class='btn btn-primary'>Tapsilog</a>
+	<a href='#' class='btn btn-success'>GAGO</a>
+	<a href='#' class='btn btn-warning'>TANGA</a>
+	<a href='#' class='btn btn-info'>TANGA</a>
+	<a href='#' class='btn btn-danger'>TANGA</a>
+	
+	<br>
+	<br>
+	
+	<button href='#' class='btn btn-danger btn-lg'>Default</button>
+	<button href='#' class='btn btn-danger btn-sm'>Default</button>
+	<button href='#' class='btn btn-inverse btn-sm'>Default</button>
+	<button href='#' class='btn btn-danger'>Default</button>
+	<button href='#' class='btn btn-danger'>Default</button>
+	<button href='#' class='btn btn-danger'>Default</button>
+	<button href='#' class='btn btn-danger'>Default</button>
+	<button href='#' class='btn btn-danger'>Default</button>
 
-    <code><?= __FILE__ ?></code>
+	<hr>
+	<button class='btn btn-primary btn-sm btn-block'>Default</button>
+	<br>
 </div> 
 
 
