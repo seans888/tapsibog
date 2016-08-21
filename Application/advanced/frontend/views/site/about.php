7743c533b8b2131c4feadab1151a582198c4605c
owenylaya <odylaya@student.apc.edu.ph>
2016-08-21 19:10:55 +0800

    MODIFIED

--- Application/advanced/frontend/views/site/about.php
+++ Application/advanced/frontend/views/site/about.php
@@ -7,6 +7,51 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+	<div class='container'>
+	
+	<a href="#" class='btn btn-default'>BUTTON</a>
+	<a href="#" class='btn btn-primary'>BUTTON</a>
+	<a href="#" class='btn btn-success'>BUTTON</a>
+	<a href="#" class='btn btn-warning'>BUTTON</a>
+	<a href="#" class='btn btn-danger'>BUTTON</a>
+	<a href="#" class='btn btn-info'>BUTTON</a>
+	<br><br>
+		<div class='container'>
+	
+	<a href="#" class='btn btn-default'>BUTTON</a>
+	<a href="#" class='btn btn-primary'>BUTTON</a>
+	<a href="#" class='btn btn-success'>BUTTON</a>
+	<a href="#" class='btn btn-warning'>BUTTON</a>
+	<a href="#" class='btn btn-danger'>BUTTON</a>
+	<a href="#" class='btn btn-info'>BUTTON</a>
+	<br><br>
+		<div class='container'>
+	
+	<a href="#" class='btn btn-default'>BUTTON</a>
+	<a href="#" class='btn btn-primary'>BUTTON</a>
+	<a href="#" class='btn btn-success'>BUTTON</a>
+	<a href="#" class='btn btn-warning'>BUTTON</a>
+	<a href="#" class='btn btn-danger'>BUTTON</a>
+	<a href="#" class='btn btn-info'>BUTTON</a>
+	<br><br>
+		<div class='container'>
+	
+	<a href="#" class='btn btn-default'>BUTTON</a>
+	<a href="#" class='btn btn-primary'>BUTTON</a>
+	<a href="#" class='btn btn-success'>BUTTON</a>
+	<a href="#" class='btn btn-warning'>BUTTON</a>
+	<a href="#" class='btn btn-danger'>BUTTON</a>
+	<a href="#" class='btn btn-info'>BUTTON</a>
+	<br><br>
+		<div class='container'>
+	
+	<a href="#" class='btn btn-default'>BUTTON</a>
+	<a href="#" class='btn btn-primary'>BUTTON</a>
+	<a href="#" class='btn btn-success'>BUTTON</a>
+	<a href="#" class='btn btn-warning'>BUTTON</a>
+	<a href="#" class='btn btn-danger'>BUTTON</a>
+	<br><br>
+	<a href="#" class='btn btn-info'>BUTTON</a>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
