7d720a5c9bf7a9ced7d51cafde3c67be75b0ee60
owenylaya <odylaya@student.apc.edu.ph>
2016-10-26 22:20:42 +0800

    stock

--- Application/stock/brand.php
+++ Application/stock/brand.php
@@ -6,12 +6,12 @@
 
 		<ol class="breadcrumb">
 		  <li><a href="dashboard.php">Home</a></li>		  
-		  <li class="active">Brand</li>
+		  <li class="active">Raw</li>
 		</ol>
 
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<div class="page-heading"> <i class="glyphicon glyphicon-edit"></i> Manage Brand</div>
+				<div class="page-heading"> <i class="glyphicon glyphicon-edit"></i> Manage Raw Items</div>
 			</div> <!-- /panel-heading -->
 			<div class="panel-body">
 
--- Application/stock/includes/header.php
+++ Application/stock/includes/header.php
@@ -54,12 +54,12 @@
       <ul class="nav navbar-nav navbar-right">        
 
       	<li id="navDashboard"><a href="index.php"><i class="glyphicon glyphicon-list-alt"></i>  Dashboard</a></li>        
-        
-        <li id="navBrand"><a href="brand.php"><i class="glyphicon glyphicon-btc"></i>  Brand</a></li>        
+         
+        <li id="navBrand"><a href="brand.php"><i class="glyphicon glyphicon-btc"></i>  Raw</a></li>        
 
         <li id="navCategories"><a href="categories.php"> <i class="glyphicon glyphicon-th-list"></i> Category</a></li>        
 
-        <li id="navProduct"><a href="product.php"> <i class="glyphicon glyphicon-ruble"></i> Product </a></li>     
+        <li id="navProduct"><a href="product.php"> <i class="glyphicon glyphicon-ruble"></i> Products </a></li>     
 
         <li class="dropdown" id="navOrder">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-shopping-cart"></i> Orders <span class="caret"></span></a>
