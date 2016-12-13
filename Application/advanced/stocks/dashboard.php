d300c52a285ad33abb0013291e34b2d3f4b72860
owenylaya <odylaya@student.apc.edu.ph>
2016-12-14 07:12:12 +0800

    haha

---
+++ Application/advanced/stocks/dashboard.php
@@ -0,0 +1,6 @@
+<?php require_once 'includes/header.php'; ?>
+<title>Tapsibog Inventory System</title>
+<center>
+<a href="orders.php?o=add"><button class="btn btn-default button1 btn-lg"  data-target="#addBrandModel"> ADD ORDER</button></a>
+
+<?php require_once 'includes/footer.php'; ?>	
